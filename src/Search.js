ed7e99527593cf3e2e40bf6193f4376a8a1cce69
bhaskarmitra5 <bhaskarmitra.zer05@gmail.com>
2018-04-18 22:32:39 +0530

    first commit, better ui and better functionality

---
+++ src/Search.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class Search extends React.Component{
+    handleSearch(e) {
+        e.preventDefault()
+        console.log(this._inputElement.value)
+        // var url="https://www.googleapis.com/customsearch/v1?key=AIzaSyBBh_URhMLG3FGGn-WFuf8Ww2g4D1sjMe4&cx=004592283957017248676:yzinsfkmf_k&q="+this._inputElement.value
+        var url="https://newsapi.org/v2/top-headlines?sources=google-news&apiKey=9471171031b84d87a2d5f77578745650"
+        console.log(url)
+        this.props.search(url)
+    }
+    render(){
+        return (
+            <form style={{textAlign:'center'}} onSubmit={this.handleSearch.bind(this)}>
+                <input style={{padding:'10px',width:'50%'}} ref={(a) => this._inputElement = a}
+                  placeholder="enter query">
+                </input>
+                <button style={{padding:'10px'}} type="submit">Search</button>
+            </form>
+
+        )
+    }
+}
+export default Search
